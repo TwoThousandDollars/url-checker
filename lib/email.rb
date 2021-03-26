f1e5f7041bcb90f9a1e96d4b79b34cb5c5132133
TwoThousandDollars <bbrooks1@harding.edu>
2021-03-25 22:54:11 -0500

    extracted audience segment info from email file name

--- lib/email.rb
+++ lib/email.rb
@@ -1,7 +1,10 @@
 require_relative "email_parser"
 
 class Email
-    def initialize(urls)
+    def initialize(email_path, urls)
+
+        # TODO()
+        # CREATE AN EMAIL OBJECT AND TEST THAT IT CAN PROPERLY EXTRACT AUDIENCE INFO FROM SL
         
         @full_audience_segment = get_full_audience_segment
         
@@ -10,14 +13,19 @@ class Email
         @module_1 = @full_audience_segment[1]
         
         @module_2 = @full_audience_segment[2]
+
+        @subject_line = @full_audience_segment[3]
         
         @urls = urls
-        
+
     end
 
     private
 
-    def get_full_audience_segment
-        return [1,2,3]
+    def get_full_audience_segment(email_file_name, audience_array=[])
+        audience_array = email_file_name.split(/\-MainEntity\-|\-M\d\-|\-proof\]\s/).slice! 0
+        audience_array.each {|i| i.downcase! }
     end
+
+
 end
